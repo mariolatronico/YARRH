1fae288f2162a725be3c77aa935c04ff1664b625
wuflnor <arek@okinet.pl>
2012-07-07 23:55:38 +0200

    added translation files

---
+++ yarrh_pl.ts
@@ -0,0 +1,437 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="aboutwindow.ui" line="14"/>
+        <source>About YARRH</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="29"/>
+        <source>YARRH: Yet Another RepRap Host</source>
+        <translation>YARRH: Yet Another RepRap Host</translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="49"/>
+        <source>v 0.1.0</source>
+        <oldsource>v 0.0.3</oldsource>
+        <translation>v 0.1.0</translation>
+    </message>
+    <message utf8="true">
+        <location filename="aboutwindow.ui" line="62"/>
+        <source>Arkadiusz Śpiewak</source>
+        <translation>Arkadiusz Śpiewak</translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="72"/>
+        <source>arek@okinet.pl</source>
+        <translation>arek@okinet.pl</translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="82"/>
+        <source>http://3d-printers.pl</source>
+        <translation>http://3d-printers.pl</translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="151"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>YARRH v0.1.0</source>
+        <translation>YARRH v0.1.0</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Baud:</source>
+        <translation>Baud:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>Name:</source>
+        <oldsource>Nazwa:</oldsource>
+        <translation>Nazwa:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="134"/>
+        <source>Needed filament:</source>
+        <oldsource>Potrzebny filament:</oldsource>
+        <translation>Potrzebny filament:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>Port settings</source>
+        <translation>Ustawienia portu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="101"/>
+        <source>Connect</source>
+        <translation>Połącz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <source>Number of layers:</source>
+        <oldsource>Number of layers</oldsource>
+        <translation>Liczba warstw:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="168"/>
+        <source>Print</source>
+        <translation>Drukuj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <location filename="mainwindow.cpp" line="210"/>
+        <location filename="mainwindow.cpp" line="246"/>
+        <source>Pause</source>
+        <oldsource>Pauza</oldsource>
+        <translation>Pauza</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="203"/>
+        <source>Temperature</source>
+        <oldsource>Temperatura</oldsource>
+        <translation>Temperatura</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="236"/>
+        <source>T1</source>
+        <translation>T1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="247"/>
+        <location filename="mainwindow.ui" line="312"/>
+        <source>230</source>
+        <translation>230</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="266"/>
+        <location filename="mainwindow.ui" line="331"/>
+        <location filename="mainwindow.ui" line="396"/>
+        <source>On</source>
+        <translation>On</translation>
+    </message>
+    <message utf8="true">
+        <location filename="mainwindow.ui" line="288"/>
+        <location filename="mainwindow.ui" line="353"/>
+        <location filename="mainwindow.ui" line="418"/>
+        <source> 0 °C</source>
+        <translation> 0 °C</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="301"/>
+        <source>T2</source>
+        <translation>T2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="366"/>
+        <source>HB</source>
+        <translation>HB</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="377"/>
+        <source>110</source>
+        <translation>110</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="428"/>
+        <source>Monitor</source>
+        <oldsource>Monitoruj</oldsource>
+        <translation>Monitoruj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="462"/>
+        <source>Axis control</source>
+        <translation>Sterowanie osiami</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="546"/>
+        <source>Go to:</source>
+        <translation>Idź do:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="595"/>
+        <source>XY speed</source>
+        <translation>Prędkość XY</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="615"/>
+        <source>Z Speed</source>
+        <translation>Prędkość Z</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="635"/>
+        <source>Fan</source>
+        <translation>Wentylator</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="655"/>
+        <source>Disable motors</source>
+        <translation>Wyłącz silniki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="711"/>
+        <source>Object view</source>
+        <translation>Podgląd obiektu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="750"/>
+        <source>Show travel moves</source>
+        <translation>Pokaż ruchy jałowe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="813"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="831"/>
+        <source>Load</source>
+        <translation>Wczytaj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="836"/>
+        <source>About YARRH</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="471"/>
+        <source>Home</source>
+        <translation>Zeruj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="484"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="497"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="510"/>
+        <source>Z</source>
+        <translation>Z</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="523"/>
+        <source>All</source>
+        <translation>Wszystkie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="539"/>
+        <source>Position</source>
+        <oldsource>Pozycja</oldsource>
+        <translation>Pozycja</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="553"/>
+        <location filename="mainwindow.ui" line="560"/>
+        <source>X: 0</source>
+        <translation>X: 0</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="567"/>
+        <location filename="mainwindow.ui" line="574"/>
+        <source>Y: 0</source>
+        <translation>Y: 0</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="581"/>
+        <location filename="mainwindow.ui" line="588"/>
+        <source>Z: 0</source>
+        <translation>Z: 0</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="602"/>
+        <location filename="mainwindow.ui" line="622"/>
+        <source> mm/min</source>
+        <translation> mm/min</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="642"/>
+        <source> %</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="775"/>
+        <source>Console</source>
+        <oldsource>Konsola</oldsource>
+        <translation type="unfinished">Konsola</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="807"/>
+        <source>Plik</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>Open file</source>
+        <translation>Otwórz plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>Print files (*.g *.gcode)</source>
+        <translation>Pliki do druku(*.g *.gcode)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="137"/>
+        <source>Parsing file</source>
+        <translation>Parsowanie pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="203"/>
+        <source>File loaded</source>
+        <translation>Plik wczytany</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="242"/>
+        <source>Resume</source>
+        <translation>Wznów</translation>
+    </message>
+</context>
+<context>
+    <name>Printer</name>
+    <message>
+        <location filename="printer.cpp" line="22"/>
+        <source>Connecting...</source>
+        <translation>Łącze...</translation>
+    </message>
+    <message>
+        <location filename="printer.cpp" line="29"/>
+        <source>Printer connected</source>
+        <translation>Drukarka połączona</translation>
+    </message>
+    <message>
+        <location filename="printer.cpp" line="34"/>
+        <source>Unable to connect</source>
+        <translation>Niemoge się połaczyć</translation>
+    </message>
+    <message>
+        <location filename="printer.cpp" line="39"/>
+        <source>Unknown port name</source>
+        <oldsource>Niepoprawny port</oldsource>
+        <translation>Nieznany port</translation>
+    </message>
+    <message>
+        <location filename="printer.cpp" line="63"/>
+        <source>Printer offline</source>
+        <translation>Drukarka niepołączona</translation>
+    </message>
+</context>
+<context>
+    <name>QextSerialPort</name>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="723"/>
+        <source>No Error has occurred</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="725"/>
+        <source>Invalid file descriptor (port was not opened correctly)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="727"/>
+        <source>Unable to allocate memory tables (POSIX)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="729"/>
+        <source>Caught a non-blocked signal (POSIX)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="731"/>
+        <source>Operation timed out (POSIX)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="733"/>
+        <source>The file opened by the port is not a valid device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="735"/>
+        <source>The port detected a break condition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="737"/>
+        <source>The port detected a framing error (usually caused by incorrect baud rate settings)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="739"/>
+        <source>There was an I/O error while communicating with the port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="741"/>
+        <source>Character buffer overrun</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="743"/>
+        <source>Receive buffer overflow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="745"/>
+        <source>The port detected a parity error in the received data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="747"/>
+        <source>Transmit buffer overflow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="749"/>
+        <source>General read operation failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="751"/>
+        <source>General write operation failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="753"/>
+        <source>The %1 file doesn&apos;t exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="755"/>
+        <source>Permission denied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="757"/>
+        <source>Device is already locked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qextserialport/qextserialport.cpp" line="759"/>
+        <source>Unknown error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
