ed55e32ebb439f1adc24a579020bdce5e214e19e
wuflnor <arek@okinet.pl>
2012-07-08 23:23:26 +0200

    Removed warning about opengl 2.0 support

--- main.cpp
+++ main.cpp
@@ -12,10 +12,5 @@ int main(int argc, char *argv[])
 
     MainWindow w;
     w.show();
-    if (!((QGLFormat::openGLVersionFlags() & QGLFormat::OpenGL_Version_2_0) ||
-               (QGLFormat::openGLVersionFlags() & QGLFormat::OpenGL_ES_Version_2_0))) {
-             QMessageBox::information(0, "ERROR","This system does not support OpenGL 2.0 or OpenGL ES 2.0");
-             return 0;
-         }
     return a.exec();
 }
